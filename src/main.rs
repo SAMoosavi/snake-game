112712890fe6c40d31687b87ff1d7b3f17d8e797
SAMoosavi <moosavi238@gmail.com>
2024-12-30 19:08:44 +0330

    create sample usage code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+mod core;
+
+use core::{Board, Direction};
+
+const N: usize = 6;
+
+fn p_array(table: &[[i8; N]; N]) {
+    for row in table {
+        println!("{:?}", row);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut a = Board::<N>::new(3).unwrap();
+    p_array(a.get_table());
+
+    a.rotation(Direction::Down);
+    while a.walk() {
+        p_array(a.get_table());
+    }
+
+    println!("game over");
+    p_array(a.get_table());
+
+    println!("{}", a.get_score());
 }
