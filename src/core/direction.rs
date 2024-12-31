e2152a8a421545bf2cb7afaf6f3f630b18519dd6
SAMoosavi <moosavi238@gmail.com>
2024-12-31 11:59:03 +0330

    add test of opposite

--- src/core/direction.rs
+++ src/core/direction.rs
@@ -16,3 +16,37 @@ impl Direction {
         )
     }
 }
+
+#[cfg(test)]
+mod test_direction {
+    use super::Direction;
+
+    #[test]
+    fn check_is_opposite() {
+        let left = Direction::Left;
+        let right = Direction::Right;
+        let up = Direction::Up;
+        let down = Direction::Down;
+
+        assert!(left.is_opposite(&right));
+        assert!(right.is_opposite(&left));
+        assert!(down.is_opposite(&up));
+        assert!(up.is_opposite(&down));
+
+        assert!(!left.is_opposite(&left));
+        assert!(!left.is_opposite(&up));
+        assert!(!left.is_opposite(&down));
+
+        assert!(!right.is_opposite(&right));
+        assert!(!right.is_opposite(&up));
+        assert!(!right.is_opposite(&down));
+
+        assert!(!up.is_opposite(&up));
+        assert!(!up.is_opposite(&left));
+        assert!(!up.is_opposite(&right));
+
+        assert!(!down.is_opposite(&down));
+        assert!(!down.is_opposite(&left));
+        assert!(!down.is_opposite(&right));
+    }
+}
